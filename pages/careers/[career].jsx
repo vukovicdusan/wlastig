4927f804de5b2630502c5e4aa8261f228ce32c68
vukovicdusan <dusan.v.vukovic@gmail.com>
2023-03-29 09:05:37 +0200

    SingleCareer style added for ul

--- pages/careers/[career].jsx
+++ pages/careers/[career].jsx
@@ -6,6 +6,7 @@ import { UnderlineStyled } from "../../components/styles/UnderlineStyled.styled"
 import AnimationContainer from "../../components/AnimationContainer"
 import { Stack } from "../../components/styles/Stack.styled"
 import { StyledText } from "../../components/styles/StyledText.styled"
+import styled from "styled-components"
 
 export async function getStaticPaths() {
 	const careersArrData = careersArr
@@ -32,48 +33,59 @@ export const getStaticProps = (context) => {
 
 const Career = ({ data }) => {
 	return (
-		<Region first={true}>
-			<Wrapper>
-				<Stack stackSpace={"var(--s5)"}>
-					<Stack>
-						<AnimationContainer>
-							<h1>{data.careerTitle}</h1>
-							<UnderlineStyled></UnderlineStyled>
-						</AnimationContainer>
-						<StyledText as={"h5"} transform={"uppercase"}>
-							{data.location} | {data.type}
-						</StyledText>
-						<p>{data.content}</p>
-					</Stack>
-					<Stack stackSpace={"var(--s3)"}>
-						<div>
+		<SingleCareer>
+			<Region first={true}>
+				<Wrapper>
+					<Stack stackSpace={"var(--s5)"}>
+						<Stack>
 							<AnimationContainer>
-								<h5>Responsibilities</h5>
+								<h1>{data.careerTitle}</h1>
 								<UnderlineStyled></UnderlineStyled>
 							</AnimationContainer>
-							<Stack as={"ul"}>
-								{data.responsibilities.map((resp, index) => (
-									<li key={index}>{resp}</li>
-								))}
-							</Stack>
-						</div>
-						<div>
-							<AnimationContainer>
-								<h5>Requirements</h5>
-								<UnderlineStyled></UnderlineStyled>
-							</AnimationContainer>
+							<StyledText as={"h5"} transform={"uppercase"}>
+								{data.location} | {data.type}
+							</StyledText>
+							<p>{data.content}</p>
+						</Stack>
+						<Stack stackSpace={"var(--s3)"}>
+							<div>
+								<AnimationContainer>
+									<h5>Responsibilities</h5>
+									<UnderlineStyled></UnderlineStyled>
+								</AnimationContainer>
+								<Stack as={"ul"}>
+									{data.responsibilities.map(
+										(resp, index) => (
+											<li key={index}>{resp}</li>
+										)
+									)}
+								</Stack>
+							</div>
+							<div>
+								<AnimationContainer>
+									<h5>Requirements</h5>
+									<UnderlineStyled></UnderlineStyled>
+								</AnimationContainer>
 
-							<Stack as={"ul"}>
-								{data.requirements.map((resp, index) => (
-									<li key={index}>{resp}</li>
-								))}
-							</Stack>
-						</div>
+								<Stack as={"ul"}>
+									{data.requirements.map((resp, index) => (
+										<li key={index}>{resp}</li>
+									))}
+								</Stack>
+							</div>
+						</Stack>
 					</Stack>
-				</Stack>
-			</Wrapper>
-		</Region>
+				</Wrapper>
+			</Region>
+		</SingleCareer>
 	)
 }
+
+export const SingleCareer = styled.div`
+	ul {
+		list-style: disc;
+		padding-inline-start: 1rem;
+	}
+`
 
 export default Career
