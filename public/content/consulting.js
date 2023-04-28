5ad6f4faa00d2773b5f99410575ebe9ab0915b4c
Picvajzmen <picvajzmen@gmail.com>
2023-04-28 09:46:55 +0200

    Fixed order of infographic titles on all services pages

--- public/content/consulting.js
+++ public/content/consulting.js
@@ -1,57 +1,56 @@
 export const processArr = [
-	{ title: "Getting to know your business" },
-	{ title: "Defining the goals" },
-	{ title: "To-Do List proposal" },
-	{ title: "Monitoring and implementation assistance" },
-	{ title: "Regular accomplishment review" },
-	{ title: "Presenting end-results" },
-]
+  { title: "Presenting end-results" },
+  { title: "Regular accomplishment review" },
+  { title: "Monitoring and implementation assistance" },
+  { title: "To-Do List proposal" },
+  { title: "Defining the goals" },
+  { title: "Getting to know your business" },
+];
 
 export const competitionArr = [
-	{
-		competition: "Focus on short-term results",
-		us: "Focus on long-term results",
-	},
-	{
-		competition: "Putting on a false front",
-		us: "Implementing the key business-growth contributing elements",
-	},
-	{
-		competition: "Cherry picking results",
-		us: "Focusing on bottom line movers",
-	},
-	{
-		competition: "Limited support",
-		us: "Full support all the way through ",
-	},
-	{
-		competition: "Inflexible approach",
-		us: "Full-flexibility approach",
-	},
-]
+  {
+    competition: "Focus on short-term results",
+    us: "Focus on long-term results",
+  },
+  {
+    competition: "Putting on a false front",
+    us: "Implementing the key business-growth contributing elements",
+  },
+  {
+    competition: "Cherry picking results",
+    us: "Focusing on bottom line movers",
+  },
+  {
+    competition: "Limited support",
+    us: "Full support all the way through ",
+  },
+  {
+    competition: "Inflexible approach",
+    us: "Full-flexibility approach",
+  },
+];
 
 export const faqArr = [
-	{
-		title: "Does consulting make sense for my business?",
-		body: (
-			<p>
-				Consulting is there to point you towards those details that
-				might have slipped your attention. Or for when you wanna try
-				something you haven’t tried before, but aren’t sure how to go
-				about it. Think Google Maps. With the right guidance, you’ll get
-				to the goal you set in no time, even though you didn’t really
-				know how to get there.
-			</p>
-		),
-	},
-	{
-		title: "When will I see results?",
-		body: (
-			<p>
-				Consulting takes time. You can’t expect results after a few days
-				or a month. (well, in some cases), but if we work together and
-				honour the agreement, the results will be visible soon enough.
-			</p>
-		),
-	},
-]
+  {
+    title: "Does consulting make sense for my business?",
+    body: (
+      <p>
+        Consulting is there to point you towards those details that might have
+        slipped your attention. Or for when you wanna try something you haven’t
+        tried before, but aren’t sure how to go about it. Think Google Maps.
+        With the right guidance, you’ll get to the goal you set in no time, even
+        though you didn’t really know how to get there.
+      </p>
+    ),
+  },
+  {
+    title: "When will I see results?",
+    body: (
+      <p>
+        Consulting takes time. You can’t expect results after a few days or a
+        month. (well, in some cases), but if we work together and honour the
+        agreement, the results will be visible soon enough.
+      </p>
+    ),
+  },
+];
--- public/content/web-analytics.js
+++ public/content/web-analytics.js
@@ -1,114 +1,111 @@
 export const subsArr = [
-	{
-		title: "Tracking",
-		content:
-			"Get an advanced tracking solution for your company's success!",
-		img: "data-analysis",
-	},
-	{
-		title: "Conversion Attribution",
-		content:
-			"Don’t go for the one-size-fits-all attribution model, get yourself a custom fit!",
-		img: "funnel",
-	},
-	{
-		title: "Website optimization",
-		content: "Transform your website into a conversion generating machine!",
-		img: "search",
-	},
-	{
-		title: "Marketing Databases",
-		content:
-			"Leverage your offline data to improve your online performance!",
-		img: "marketing",
-	},
-	{
-		title: "Predictive analytics and ML",
-		content:
-			"Predicting and capitalizing on the future trends and customer behaviour!",
-		img: "report",
-	},
-	{
-		title: "A/B testing",
-		content: "Test your hypothesis to improve your results!",
-		img: "testing",
-	},
-	{
-		title: "Reporting and Visualization",
-		content: "Uncover the story behind your data!",
-		img: "chart",
-	},
-	{
-		title: "Data Quality Testing",
-		content:
-			"Your decisions are only as good as your data. Improve your decisions by enhancing the quality of your data.",
-		img: "direction",
-	},
-	{
-		title: "Behavior Analysis",
-		content:
-			"Understand who your customers are, what they want and what impacts their decision making.",
-		img: "customer",
-	},
-]
+  {
+    title: "Tracking",
+    content: "Get an advanced tracking solution for your company's success!",
+    img: "data-analysis",
+  },
+  {
+    title: "Conversion Attribution",
+    content:
+      "Don’t go for the one-size-fits-all attribution model, get yourself a custom fit!",
+    img: "funnel",
+  },
+  {
+    title: "Website optimization",
+    content: "Transform your website into a conversion generating machine!",
+    img: "search",
+  },
+  {
+    title: "Marketing Databases",
+    content: "Leverage your offline data to improve your online performance!",
+    img: "marketing",
+  },
+  {
+    title: "Predictive analytics and ML",
+    content:
+      "Predicting and capitalizing on the future trends and customer behaviour!",
+    img: "report",
+  },
+  {
+    title: "A/B testing",
+    content: "Test your hypothesis to improve your results!",
+    img: "testing",
+  },
+  {
+    title: "Reporting and Visualization",
+    content: "Uncover the story behind your data!",
+    img: "chart",
+  },
+  {
+    title: "Data Quality Testing",
+    content:
+      "Your decisions are only as good as your data. Improve your decisions by enhancing the quality of your data.",
+    img: "direction",
+  },
+  {
+    title: "Behavior Analysis",
+    content:
+      "Understand who your customers are, what they want and what impacts their decision making.",
+    img: "customer",
+  },
+];
 
 export const processArr = [
-	{ title: "Understanding Your Business" },
-	{ title: "Tracking and Data Portfolio Audit" },
-	{ title: "Fixing and upgrading existing tracking structure" },
-	{ title: "Analyzing for valuable insights" },
-	{ title: "Reporting and Data visualisation" },
-	{ title: "Continual testing for constant improvement" },
-]
+  { title: "Continual testing for constant improvement" },
+  { title: "Reporting and Data visualisation" },
+  { title: "Analyzing for valuable insights" },
+  { title: "Fixing and upgrading existing tracking structure" },
+  { title: "Tracking and Data Portfolio Audit" },
+  { title: "Understanding Your Business" },
+];
 
 export const competitionArr = [
-	{
-		competition: "One-size-fits-all solutions",
-		us: "Tailor-made solutions to fit your specific needs",
-	},
-	{
-		competition: "No technical implementation documentation",
-		us: "In-depth, step by step procedure sheet",
-	},
-	{
-		competition: "Set-it-and-forget-it approach",
-		us: "Continuous testing and data quality improvement",
-	},
-	{
-		competition: "Vanity metrics ego boost reporting",
-		us: "Profit-oriented actionable insights ",
-	},
-	{
-		competition: "Inefficient tracking setup slowing down your website",
-		us: "Website-friendly tracking solutions",
-	},
-	{
-		competition: "Hiding behind the technical jargon",
-		us: "Transparent communication",
-	},
-	{ competition: "10 accounts per person", us: "3 accounts per person" },
-]
+  {
+    competition: "One-size-fits-all solutions",
+    us: "Tailor-made solutions to fit your specific needs",
+  },
+  {
+    competition: "No technical implementation documentation",
+    us: "In-depth, step by step procedure sheet",
+  },
+  {
+    competition: "Set-it-and-forget-it approach",
+    us: "Continuous testing and data quality improvement",
+  },
+  {
+    competition: "Vanity metrics ego boost reporting",
+    us: "Profit-oriented actionable insights ",
+  },
+  {
+    competition: "Inefficient tracking setup slowing down your website",
+    us: "Website-friendly tracking solutions",
+  },
+  {
+    competition: "Hiding behind the technical jargon",
+    us: "Transparent communication",
+  },
+  { competition: "10 accounts per person", us: "3 accounts per person" },
+];
 
 export const faqArr = [
-	{
-		title: "How much experience do you have?",
-		body: (
-			<p>
-				The team that will help your business grow learned their craft
-				working for more than 200 clients, coming from a wide array of
-				business verticals with diverse professional needs.
-			</p>
-		),
-	},
-	{
-		title: "What is your specialty?",
-		body: (
-			<p>
-				Data Collection.<br></br>
-				<br></br> Make sure that the data portfolio contains all
-				valuable data touchpoints and that you have the highest data
-				quality possible.
-			</p>
-		),
-	},
-]
+  {
+    title: "How much experience do you have?",
+    body: (
+      <p>
+        The team that will help your business grow learned their craft working
+        for more than 200 clients, coming from a wide array of business
+        verticals with diverse professional needs.
+      </p>
+    ),
+  },
+  {
+    title: "What is your specialty?",
+    body: (
+      <p>
+        Data Collection.<br></br>
+        <br></br> Make sure that the data portfolio contains all valuable data
+        touchpoints and that you have the highest data quality possible.
+      </p>
+    ),
+  },
+];
