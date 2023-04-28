3af7f3f605a286b13bd71b8c3eba7ca2663d2377
Picvajzmen <picvajzmen@gmail.com>
2023-04-28 11:17:05 +0200

    Added audit form to the Our team page at the bottom

--- components/ContactFormSection.jsx
+++ components/ContactFormSection.jsx
@@ -1,67 +1,68 @@
-import React, { useEffect, useState } from "react"
-import Contact from "./Contact"
-import { Shapedivider } from "./styles/Shapedivider.styled"
-import { FullBackground } from "./styles/FullBackground.styled"
-import { Wrapper } from "./styles/Wrapper.styled"
-import { Region } from "./styles/Region.styled"
-import { Switcher } from "./styles/Switcher.styled"
-import { Stack } from "./styles/Stack.styled"
-import styled from "styled-components"
-import AnimationContainer from "./AnimationContainer"
+import React, { useEffect, useState } from "react";
+import Contact from "./Contact";
+import { Shapedivider } from "./styles/Shapedivider.styled";
+import { FullBackground } from "./styles/FullBackground.styled";
+import { Wrapper } from "./styles/Wrapper.styled";
+import { Region } from "./styles/Region.styled";
+import { Switcher } from "./styles/Switcher.styled";
+import { Stack } from "./styles/Stack.styled";
+import styled from "styled-components";
+import AnimationContainer from "./AnimationContainer";
 
 const ContactFormSection = (props) => {
-	return (
-		<FullBackground background={"var(--primary)"}>
-			{props.shapedivider ? (
-				<Shapedivider
-					height={"130px"}
-					rotation={"0"}
-					fill={props.shapedividerColor || "var(--background-light)"}
-				>
-					<svg
-						data-name="Layer 1"
-						xmlns="http://www.w3.org/2000/svg"
-						viewBox="0 0 1200 120"
-						preserveAspectRatio="none"
-					>
-						<path d="M0,0V7.23C0,65.52,268.63,112.77,600,112.77S1200,65.52,1200,7.23V0Z"></path>
-					</svg>
-				</Shapedivider>
-			) : null}
-			<Region>
-				<Wrapper>
-					<ContactFormSectionStyled>
-						<Switcher
-							reverse={"row-reverse"}
-							switcherJustify={"center"}
-							gap={"var(--s5)"}
-						>
-							<Stack
-								stackJustify={"center"}
-								stackAlign={"center"}
-							>
-								<AnimationContainer>
-									<h3 className="title-xl">{props.title}</h3>
-									<p className="title-l">{props.subTitle}</p>
-								</AnimationContainer>
-							</Stack>
-							<Contact cta={props.cta}></Contact>
-						</Switcher>
-					</ContactFormSectionStyled>
-				</Wrapper>
-			</Region>
-		</FullBackground>
-	)
-}
+  return (
+    <FullBackground background={"var(--primary)"}>
+      {props.shapedivider ? (
+        <Shapedivider
+          height={"130px"}
+          rotation={"0"}
+          fill={props.shapedividerColor || "var(--background-light)"}
+        >
+          <svg
+            data-name="Layer 1"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 1200 120"
+            preserveAspectRatio="none"
+          >
+            <path d="M0,0V7.23C0,65.52,268.63,112.77,600,112.77S1200,65.52,1200,7.23V0Z"></path>
+          </svg>
+        </Shapedivider>
+      ) : null}
+      <Region>
+        <Wrapper>
+          <ContactFormSectionStyled>
+            <Switcher
+              reverse={"row-reverse"}
+              switcherJustify={"center"}
+              gap={"var(--s5)"}
+            >
+              <Stack stackJustify={"center"} stackAlign={"center"}>
+                <AnimationContainer>
+                  <h3 className="title-xl">
+                    {props.title || "150+ Point Audit For Data Quality"}
+                  </h3>
+                  <p className="title-l">
+                    {props.subTitle || "(3X The Industry Average)"}
+                  </p>
+                </AnimationContainer>
+              </Stack>
+              <Contact cta={props.cta}></Contact>
+            </Switcher>
+          </ContactFormSectionStyled>
+        </Wrapper>
+      </Region>
+    </FullBackground>
+  );
+};
 
 export const ContactFormSectionStyled = styled.div`
-	margin-block-start: var(--s5);
+  margin-block-start: var(--s5);
 
-	& .title-xl,
-	& .title-l {
-		text-align: center;
-		color: var(--text-light);
-	}
-`
+  & .title-xl,
+  & .title-l {
+    text-align: center;
+    color: var(--text-light);
+  }
+`;
 
-export default ContactFormSection
+export default ContactFormSection;
--- pages/our-team.jsx
+++ pages/our-team.jsx
@@ -1,132 +1,130 @@
-import React from "react"
-import styled from "styled-components"
-import AnimationContainer from "../components/AnimationContainer"
-import Image from "next/image"
-import { Region } from "../components/styles/Region.styled"
-import { ServicesHeroStyled } from "../components/styles/services/ServicesHeroStyled.styled"
-import { Stack } from "../components/styles/Stack.styled"
-import { Switcher } from "../components/styles/Switcher.styled"
-import { Wrapper } from "../components/styles/Wrapper.styled"
-import { teamArr } from "../public/content/team"
-import { FullBackground } from "../components/styles/FullBackground.styled"
-import { Shapedivider } from "../components/styles/Shapedivider.styled"
-import Head from "next/head"
-import Modal from "../components/Modal"
+import React from "react";
+import styled from "styled-components";
+import AnimationContainer from "../components/AnimationContainer";
+import Image from "next/image";
+import { Region } from "../components/styles/Region.styled";
+import { ServicesHeroStyled } from "../components/styles/services/ServicesHeroStyled.styled";
+import { Stack } from "../components/styles/Stack.styled";
+import { Switcher } from "../components/styles/Switcher.styled";
+import { Wrapper } from "../components/styles/Wrapper.styled";
+import { teamArr } from "../public/content/team";
+import { FullBackground } from "../components/styles/FullBackground.styled";
+import { Shapedivider } from "../components/styles/Shapedivider.styled";
+import Head from "next/head";
+import Modal from "../components/Modal";
+import ContactFormSection from "../components/ContactFormSection";
 
 const ourTeam = () => {
-	return (
-		<>
-			<Head>
-				<title>Wlastig Analytics - Our Team</title>
-				<meta
-					name="description"
-					content="Wanna hear a great story?
+  return (
+    <>
+      <Head>
+        <title>Wlastig Analytics - Our Team</title>
+        <meta
+          name="description"
+          content="Wanna hear a great story?
 					A moving story about a brave company that made use of data on her way to become a market leader and a household brand. What company are we talking about? Yours!"
-				/>
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
-			<ServicesHeroStyled backgroundImg={"./img/pages/hero-team.png"}>
-				<div className="hero-background">
-					<Region>
-						<Wrapper>
-							<div className="hero-content ">
-								<Stack stackAlign={"end"}>
-									<AnimationContainer>
-										<h1>Meet our team!</h1>
-									</AnimationContainer>
-								</Stack>
-							</div>
-						</Wrapper>
-					</Region>
-				</div>
-			</ServicesHeroStyled>
-			<OurTeamStyled>
-				<FullBackground background={"var(--primary-shade)"}>
-					<Region pt={"0"}>
-						{teamArr.map((member, index) => (
-							<FullBackground
-								key={index}
-								background={
-									index % 2 !== 0
-										? "var(--primary-shade)"
-										: "var(--background-light)"
-								}
-							>
-								<Wrapper>
-									<Switcher
-										className="member-container"
-										reverse={index % 2 === 0 ? true : false}
-									>
-										<div className="img-container">
-											<Image
-												width={350}
-												height={300}
-												src={member.img}
-												alt="team member"
-											></Image>
-										</div>
-										<Stack
-											className="member-content-container"
-											stackJustify={"center"}
-											stackAlign={"flex-start"}
-										>
-											<h4>{member.name}</h4>
-											{member.content}
-										</Stack>
-									</Switcher>
-								</Wrapper>
-							</FullBackground>
-						))}
-					</Region>
-				</FullBackground>
-				<Shapedivider
-					position={"bottom"}
-					rotation={"0"}
-					height={"80px"}
-				>
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						viewBox="0 0 1200 120"
-						preserveAspectRatio="none"
-					>
-						<path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"></path>
-					</svg>
-				</Shapedivider>
-			</OurTeamStyled>
-			<Modal></Modal>
-		</>
-	)
-}
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <ServicesHeroStyled backgroundImg={"./img/pages/hero-team.png"}>
+        <div className="hero-background">
+          <Region>
+            <Wrapper>
+              <div className="hero-content ">
+                <Stack stackAlign={"end"}>
+                  <AnimationContainer>
+                    <h1>Meet our team!</h1>
+                  </AnimationContainer>
+                </Stack>
+              </div>
+            </Wrapper>
+          </Region>
+        </div>
+      </ServicesHeroStyled>
+      <OurTeamStyled>
+        <FullBackground background={"var(--primary-shade)"}>
+          <Region pt={"0"}>
+            {teamArr.map((member, index) => (
+              <FullBackground
+                key={index}
+                background={
+                  index % 2 !== 0
+                    ? "var(--primary-shade)"
+                    : "var(--background-light)"
+                }
+              >
+                <Wrapper>
+                  <Switcher
+                    className="member-container"
+                    reverse={index % 2 === 0 ? true : false}
+                  >
+                    <div className="img-container">
+                      <Image
+                        width={350}
+                        height={300}
+                        src={member.img}
+                        alt="team member"
+                      ></Image>
+                    </div>
+                    <Stack
+                      className="member-content-container"
+                      stackJustify={"center"}
+                      stackAlign={"flex-start"}
+                    >
+                      <h4>{member.name}</h4>
+                      {member.content}
+                    </Stack>
+                  </Switcher>
+                </Wrapper>
+              </FullBackground>
+            ))}
+          </Region>
+        </FullBackground>
+        <Shapedivider position={"bottom"} rotation={"0"} height={"80px"}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 1200 120"
+            preserveAspectRatio="none"
+          >
+            <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"></path>
+          </svg>
+        </Shapedivider>
+      </OurTeamStyled>
+      <ContactFormSection></ContactFormSection>
+      <Modal></Modal>
+    </>
+  );
+};
 
 export const OurTeamStyled = styled.div`
-	position: relative;
-	img {
-		object-fit: contain;
-		margin-inline: auto;
-	}
+  position: relative;
+  img {
+    object-fit: contain;
+    margin-inline: auto;
+  }
 
-	.img-container {
-		display: flex;
-		align-items: flex-end;
-	}
+  .img-container {
+    display: flex;
+    align-items: flex-end;
+  }
 
-	.shapedivider {
-		z-index: -1;
-	}
+  .shapedivider {
+    z-index: -1;
+  }
 
-	.member-container {
-		padding-block-start: 2rem;
-	}
+  .member-container {
+    padding-block-start: 2rem;
+  }
 
-	.member-content-container {
-		padding-block-end: 1rem;
-	}
+  .member-content-container {
+    padding-block-end: 1rem;
+  }
 
-	@media (max-width: 700px) {
-		.member-content-container:last-child {
-			padding-block-end: var(--s4);
-		}
-	}
-`
+  @media (max-width: 700px) {
+    .member-content-container:last-child {
+      padding-block-end: var(--s4);
+    }
+  }
+`;
 
-export default ourTeam
+export default ourTeam;
