515a8dbcca52c7fafafac0a22087afdbb4a762b5
Picvajzmen <picvajzmen@gmail.com>
2023-04-28 11:14:15 +0200

    Our team row-reverse bug fixed - document

--- components/styles/Switcher.styled.jsx
+++ components/styles/Switcher.styled.jsx
@@ -1,24 +1,22 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 export const Switcher = styled.div`
-	margin-block-start: ${(props) => props.mt || null};
-	margin-block-end: ${(props) => props.mb || null};
-	display: flex;
-	flex-wrap: wrap;
-	gap: ${(props) => props.gap || "var(--s1)"};
-	justify-content: ${(props) => props.switcherJustify || "flex-start"};
-	align-items: ${(props) => props.switcherAlign || ""};
-	flex-direction: ${(props) => props.reverse || "revert"};
+  margin-block-start: ${(props) => props.mt || null};
+  margin-block-end: ${(props) => props.mb || null};
+  display: flex;
+  flex-wrap: wrap;
+  gap: ${(props) => props.gap || "var(--s1)"};
+  justify-content: ${(props) => props.switcherJustify || "flex-start"};
+  align-items: ${(props) => props.switcherAlign || ""};
+  flex-direction: ${(props) => (props.reverse ? "row-reverse" : "revert")};
 
-	& > * {
-		flex-grow: 1;
-		flex-basis: calc(
-			(${(props) => props.flexBasis || "50rem"} - 100%) * 999
-		);
-	}
+  & > * {
+    flex-grow: 1;
+    flex-basis: calc((${(props) => props.flexBasis || "50rem"} - 100%) * 999);
+  }
 
-	& > :nth-last-child(n + ${(props) => props.elCount + 1 || 3}),
-	& > :nth-last-child(n + ${(props) => props.elCount + 1 || 3}) ~ * {
-		flex-basis: 100%;
-	}
-`
+  & > :nth-last-child(n + ${(props) => props.elCount + 1 || 3}),
+  & > :nth-last-child(n + ${(props) => props.elCount + 1 || 3}) ~ * {
+    flex-basis: 100%;
+  }
+`;
