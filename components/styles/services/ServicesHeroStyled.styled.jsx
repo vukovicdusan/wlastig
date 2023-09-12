5313b3f394c94c07372e3c8514fad2488300af63
Picvajzmen <picvajzmen@gmail.com>
2023-09-12 12:37:41 +0200

    Hero background min height changed to 550px

--- components/styles/services/ServicesHeroStyled.styled.jsx
+++ components/styles/services/ServicesHeroStyled.styled.jsx
@@ -1,65 +1,65 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 export const ServicesHeroStyled = styled.div`
-	color: var(--text-light);
-	margin-block-start: 65px;
-	.hero-background {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		background-image: url(${(props) => props.backgroundImg || ""});
-		background-repeat: no-repeat;
-		background-size: cover;
-		background-position: ${(props) => props.deskPos || "15%"};
-		min-height: 400px;
-	}
+  color: var(--text-light);
+  margin-block-start: 65px;
+  .hero-background {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-image: url(${(props) => props.backgroundImg || ""});
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: ${(props) => props.deskPos || "15%"};
+    min-height: 550px;
+  }
 
-	.hero-content {
-		text-align: end;
-		max-width: 50%;
-		margin-inline-start: auto;
-	}
+  .hero-content {
+    text-align: end;
+    max-width: 50%;
+    margin-inline-start: auto;
+  }
 
-	.service-info {
-		color: var(--text-dark);
-	}
+  .service-info {
+    color: var(--text-dark);
+  }
 
-	.mobile {
-		display: none;
-	}
+  .mobile {
+    display: none;
+  }
 
-	@media (max-width: 850px) {
-		.desktop {
-			display: none;
-		}
+  @media (max-width: 850px) {
+    .desktop {
+      display: none;
+    }
 
-		h1 {
-			font-size: var(--s2);
-		}
+    h1 {
+      font-size: var(--s2);
+    }
 
-		.mobile {
-			display: block;
-		}
+    .mobile {
+      display: block;
+    }
 
-		.mobile button {
-			display: block;
-			margin-inline: auto;
-		}
+    .mobile button {
+      display: block;
+      margin-inline: auto;
+    }
 
-		.hero-background {
-			min-height: 250px;
-			background-position: ${(props) => props.tabPos || "30%"};
-		}
-	}
+    .hero-background {
+      min-height: 250px;
+      background-position: ${(props) => props.tabPos || "30%"};
+    }
+  }
 
-	@media (max-width: 450px) {
-		.hero-background {
-			background-position: ${(props) => props.mobPos || "40%"};
-			min-height: 150px;
-		}
+  @media (max-width: 450px) {
+    .hero-background {
+      background-position: ${(props) => props.mobPos || "40%"};
+      min-height: 150px;
+    }
 
-		h1 {
-			font-size: var(--s0);
-		}
-	}
-`
+    h1 {
+      font-size: var(--s0);
+    }
+  }
+`;
