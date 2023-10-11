05dcea09f1fb44e08825459190c58b687bf38bf1
Picvajzmen <picvajzmen@gmail.com>
2023-10-11 09:12:40 +0200

    Changed the svg on shapedivider on the bottom of the our team page so that the last img is not floating

--- pages/our-team.jsx
+++ pages/our-team.jsx
@@ -90,13 +90,14 @@ const ourTeam = () => {
             ))}
           </Region>
         </FullBackground>
-        <Shapedivider position={"bottom"} rotation={"0"} height={"80px"}>
+        <Shapedivider position={"bottom"} rotation={"180"} height={"110px"}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 1200 120"
             preserveAspectRatio="none"
           >
-            <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"></path>
+            {/* <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"></path> */}
+            <path d="M1200 120L0 16.48 0 0 1200 0 1200 120z"></path>
           </svg>
         </Shapedivider>
       </OurTeamStyled>
