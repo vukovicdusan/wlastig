eede241ca87506afaf08614f8c0dd5a209de18f6
Picvajzmen <picvajzmen@gmail.com>
2023-11-07 10:51:36 +0100

    Added gtm script to the Head

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,12 +3,22 @@ import "../components/layout/Layout";
 import Layout from "../components/layout/Layout";
 import Head from "next/head";
 import { ModalCtxProvider } from "../store/ModalCtx";
-import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-P3TMF2H');
+            `,
+          }}
+        />
         <link
           rel="preload"
           href="/fonts/poppins-extrabold-webfont.woff2"
@@ -30,18 +40,8 @@ function MyApp({ Component, pageProps }) {
           type="font/woff2"
           crossOrigin="anonymous"
         />
-        {/* <!-- End Google Tag Manager --> */}
       </Head>
-      {/* <!-- Google Tag Manager --> */}
-      <Script id="google-tag-manager" strategy="afterInteractive">
-        {`
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-		'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-		})(window,document,'script','dataLayer','GTM-P3TMF2H');
-      `}
-      </Script>
+
       <GlobalStyles></GlobalStyles>
       <ModalCtxProvider>
         <Layout>
