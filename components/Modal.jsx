5bef0a817001fef47bd34bcde2ac8aa1c9052493
Picvajzmen <picvajzmen@gmail.com>
2023-11-08 09:31:38 +0100

    Popup small displays cutof fix

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -6,136 +6,132 @@ import Audit from "./Audit";
 import ContactForm from "./ContactForm";
 
 const Modal = () => {
-	const modalCtx = useContext(ModalCtx);
-	const router = useRouter();
+  const modalCtx = useContext(ModalCtx);
+  const router = useRouter();
 
-	const [modalOpen, setModalOpen] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
 
-	useEffect(() => {
-		if (!modalCtx.auditSent) {
-			modalCtx.forceOpen.state ? setModalOpen(true) : setModalOpen(false);
-		}
-	}, [modalCtx.forceOpen.state]);
+  useEffect(() => {
+    if (!modalCtx.auditSent) {
+      modalCtx.forceOpen.state ? setModalOpen(true) : setModalOpen(false);
+    }
+  }, [modalCtx.forceOpen.state]);
 
-	useEffect(() => {
-		modalOpen
-			? (document.body.style.overflow = "hidden")
-			: (document.body.style.overflow = "auto");
-	}, [modalOpen]);
+  useEffect(() => {
+    modalOpen
+      ? (document.body.style.overflow = "hidden")
+      : (document.body.style.overflow = "auto");
+  }, [modalOpen]);
 
-	useEffect(() => {
-		if (Object.keys(router.components).length === 3) {
-			if (!modalCtx.hasOpened) {
-				if (!modalCtx.auditSent) {
-					setModalOpen(true);
-					modalCtx.modalOpenedHandler();
-				}
-			}
-		}
-	}, [router, modalCtx]);
+  useEffect(() => {
+    if (Object.keys(router.components).length === 3) {
+      if (!modalCtx.hasOpened) {
+        if (!modalCtx.auditSent) {
+          setModalOpen(true);
+          modalCtx.modalOpenedHandler();
+        }
+      }
+    }
+  }, [router, modalCtx]);
 
-	return (
-		<>
-			{modalOpen ? (
-				<ModalStyled show={modalOpen}>
-					<dialog>
-						<span
-							onClick={() => {
-								modalCtx.modalCloseHandler(),
-									setModalOpen(false);
-							}}
-							className="modal-close"
-						>
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								width="24"
-								height="24"
-								viewBox="0 0 24 24"
-								fill="#f8f8ff"
-							>
-								<path d="M23 20.168l-8.185-8.187 8.185-8.174-2.832-2.807-8.182 8.179-8.176-8.179-2.81 2.81 8.186 8.196-8.186 8.184 2.81 2.81 8.203-8.192 8.18 8.192z" />
-							</svg>
-						</span>
-						{modalCtx.forceOpen?.type === "contact" &&
-						modalCtx.forceOpen.state ? (
-							<ContactForm></ContactForm>
-						) : (
-							<Audit
-								popup={true}
-								cta={"Get Your Free Audit"}
-							></Audit>
-						)}
-					</dialog>
-				</ModalStyled>
-			) : null}
-		</>
-	);
+  return (
+    <>
+      {modalOpen ? (
+        <ModalStyled show={modalOpen}>
+          <dialog>
+            <span
+              onClick={() => {
+                modalCtx.modalCloseHandler(), setModalOpen(false);
+              }}
+              className="modal-close"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="#f8f8ff"
+              >
+                <path d="M23 20.168l-8.185-8.187 8.185-8.174-2.832-2.807-8.182 8.179-8.176-8.179-2.81 2.81 8.186 8.196-8.186 8.184 2.81 2.81 8.203-8.192 8.18 8.192z" />
+              </svg>
+            </span>
+            {modalCtx.forceOpen?.type === "contact" &&
+            modalCtx.forceOpen.state ? (
+              <ContactForm></ContactForm>
+            ) : (
+              <Audit popup={true} cta={"Get Your Free Audit"}></Audit>
+            )}
+          </dialog>
+        </ModalStyled>
+      ) : null}
+    </>
+  );
 };
 
 export const ModalStyled = styled.div`
-	position: fixed;
-	inset-block-start: 50%;
-	inset-inline-start: 50%;
-	transform: translate(-50%, -50%);
-	background-color: rgba(0, 0, 0, 0.7);
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	width: 100vw;
-	height: 100vh;
-	z-index: 10;
-	overflow: auto;
-	opacity: 0;
-	animation: ${(props) => (props.show ? "fadeIn .4s forwards" : "")};
-	transition: all 0.4s ease-in;
+  position: fixed;
+  top: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.7);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  z-index: 10;
+  overflow: auto;
+  opacity: 0;
+  animation: ${(props) => (props.show ? "fadeIn .4s forwards" : "")};
+  transition: all 0.4s ease-in;
 
-	@keyframes fadeIn {
-		0% {
-			opacity: 0;
-		}
-		100% {
-			opacity: 1;
-		}
-	}
+  @keyframes fadeIn {
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
 
-	dialog {
-		display: block;
-		background-color: var(--background-light);
-		color: revert;
-		border: none;
-		border-radius: 5px;
-		/* padding: var(--s2) var(--s1); */
-		margin-block: auto;
-		animation: ${(props) => (props.show ? "slideDown .8s forwards" : "")};
-	}
+  dialog {
+    position: relative;
+    display: block;
+    background-color: var(--background-light);
+    color: revert;
+    border: none;
+    border-radius: 5px;
+    /* padding: var(--s2) var(--s1); */
+    margin-block: auto;
+    animation: ${(props) => (props.show ? "slideDown .8s forwards" : "")};
+  }
 
-	@keyframes slideDown {
-		0% {
-			opacity: 0;
-			transform: translateY(-500px);
-		}
-		100% {
-			transform: translateY(0px);
-			opacity: 1;
-		}
-	}
+  @keyframes slideDown {
+    0% {
+      opacity: 0;
+      transform: translateY(-500px);
+    }
+    100% {
+      transform: translateY(0px);
+      opacity: 1;
+    }
+  }
 
-	.modal-close {
-		position: absolute;
-		top: 5%;
-		right: 5%;
-		z-index: 10;
-		cursor: pointer;
-	}
+  .modal-close {
+    position: absolute;
+    top: 5%;
+    right: 5%;
+    z-index: 10;
+    cursor: pointer;
+  }
 
-	.modal-close svg {
-		fill: var(--secondary);
-		transition: fill 0.2s ease;
-	}
+  .modal-close svg {
+    fill: var(--secondary);
+    transition: fill 0.2s ease;
+  }
 
-	.modal-close svg:hover {
-		fill: var(--primary);
-	}
+  .modal-close svg:hover {
+    fill: var(--primary);
+  }
 `;
 
 export default Modal;
