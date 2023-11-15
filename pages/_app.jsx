9f9d002fee7245b047af755b75f69952748dc47f
vukovicdusan <dusan.v.vukovic@gmail.com>
2023-11-15 19:52:49 +0100

    Added next/script component for gtm to the app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,49 +3,51 @@ import "../components/layout/Layout";
 import Layout from "../components/layout/Layout";
 import Head from "next/head";
 import { ModalCtxProvider } from "../store/ModalCtx";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
+	return (
+		<>
+			<Head>
+				<link
+					rel="preload"
+					href="/fonts/poppins-extrabold-webfont.woff2"
+					as="font"
+					type="font/woff2"
+					crossOrigin="anonymous"
+				/>
+				<link
+					rel="preload"
+					href="/fonts/poppins-medium-webfont.woff2"
+					as="font"
+					type="font/woff2"
+					crossOrigin="anonymous"
+				/>
+				<link
+					rel="preload"
+					href="/fonts/poppins-regular-webfont.woff2"
+					as="font"
+					type="font/woff2"
+					crossOrigin="anonymous"
+				/>
+			</Head>
+			<Script
+				id="gtm"
+				dangerouslySetInnerHTML={{
+					__html: `
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://load.data.wlastig.com/yousqvsq.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-P3TMF2H');
             `,
-          }}
-        />
-        <link
-          rel="preload"
-          href="/fonts/poppins-extrabold-webfont.woff2"
-          as="font"
-          type="font/woff2"
-          crossOrigin="anonymous"
-        />
-        <link
-          rel="preload"
-          href="/fonts/poppins-medium-webfont.woff2"
-          as="font"
-          type="font/woff2"
-          crossOrigin="anonymous"
-        />
-        <link
-          rel="preload"
-          href="/fonts/poppins-regular-webfont.woff2"
-          as="font"
-          type="font/woff2"
-          crossOrigin="anonymous"
-        />
-      </Head>
+				}}
+			/>
 
-      <GlobalStyles></GlobalStyles>
-      <ModalCtxProvider>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </ModalCtxProvider>
-    </>
-  );
+			<GlobalStyles></GlobalStyles>
+			<ModalCtxProvider>
+				<Layout>
+					<Component {...pageProps} />
+				</Layout>
+			</ModalCtxProvider>
+		</>
+	);
 }
 
 export default MyApp;
