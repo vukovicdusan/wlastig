18e15a6ab8c4229c547b2471ca24177e2841f03c
Picvajzmen <picvajzmen@gmail.com>
2024-03-28 09:37:11 +0100

    Added confirmation dialog for RemoveFirebaseButton component

--- components/RemoveFirebaseButton.jsx
+++ components/RemoveFirebaseButton.jsx
@@ -1,36 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { removeDocumentHandler } from "../helpers/removeDocumentHandler";
 
 const RemoveFirebaseButton = (props) => {
+  const [confirmation, setConfirmation] = useState(false);
+
   const onRemoveItemHandler = () => {
     removeDocumentHandler(props.document, props.id);
     const list = props.list.filter((item) => item.id !== props.id);
     props.returnListHandler(list);
+    setConfirmation(false);
   };
 
   return (
-    <StyledRemoveButton onClick={onRemoveItemHandler}>
-      <svg className="icon">
-        <use xlinkHref={"./img/subservices/sprite.svg#remove-folder"}></use>
-      </svg>
+    <StyledRemoveButton>
+      <button onClick={() => setConfirmation(true)}>
+        <svg className="icon">
+          <use xlinkHref={"./img/subservices/sprite.svg#remove-folder"}></use>
+        </svg>
+      </button>
+      {confirmation ? (
+        <div className="confirmation-dialog">
+          <div>
+            <span>Are you sure?</span>
+            <button onClick={onRemoveItemHandler}>Yes</button>
+            <button onClick={() => setConfirmation(false)}>No</button>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
     </StyledRemoveButton>
   );
 };
 
 export default RemoveFirebaseButton;
 
-export const StyledRemoveButton = styled.button`
+export const StyledRemoveButton = styled.div`
   background-color: transparent;
   border: none;
   & .icon {
     fill: var(--background-dark);
-    width: 20px;
-    height: 20px;
-    cursor: pointer;
+    width: 20px !important;
+    height: 20px !important;
   }
 
   & .icon:hover {
     fill: var(--error-color) !important;
+  }
+
+  & button {
+    cursor: pointer;
+  }
+
+  & button:first-child {
+    border: none;
+    background-color: transparent;
+  }
+
+  .confirmation-dialog {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.3);
+    z-index: 101;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .confirmation-dialog div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: var(--background-light);
+    padding: 2rem;
+    gap: 0.5rem;
+  }
+
+  .confirmation-dialog div > *:nth-child(2):hover {
+    color: var(--success-color);
+  }
+
+  .confirmation-dialog div > *:nth-child(3):hover {
+    color: var(--error-color);
   }
 `;
