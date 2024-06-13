7d2780dcd50bd3f5c516a02b70d8bb05283f6866
Picvajzmen <picvajzmen@gmail.com>
2024-06-13 12:50:55 +0200

    implemented new klaviyo api

--- pages/api/klaviyo.js
+++ pages/api/klaviyo.js
@@ -2,47 +2,44 @@ const handler = async (req, res) => {
   const api_key = process.env.KLAVIYO_API;
   const contact_form_list_id = "WLjUJC";
   const free_consultation = "XZQiee";
+
   try {
-    const { name, email, type } = req.body.data;
-
-    // const options = {
-    //   method: "POST",
-    //   headers: {
-    //     accept: "application/json",
-    //     revision: "2024-05-15",
-    //     "content-type": "application/json",
-    //     Authorization: `Klaviyo-API-Key ${api_key}`,
-    //   },
-    //   body: JSON.stringify({
-    //     data: [{ type: "profile" }, { name: name }, { email: email }],
-    //   }),
-    // };
-
-    // const listId =
-    //   type === "contact" ? contact_form_list_id : free_consultation;
-
-    // const response = await fetch(
-    //   `https://a.klaviyo.com/api/lists/${listId}/relationships/profiles/`,
-    //   options
-    // );
-
+    const { name, email, type, website } = req.body.data;
+    const listId =
+      type === "contact" ? contact_form_list_id : free_consultation;
     const options = {
       method: "POST",
       headers: {
         accept: "application/json",
+        revision: "2024-05-15",
         "content-type": "application/json",
+        Authorization: `Klaviyo-API-Key ${api_key}`,
       },
       body: JSON.stringify({
-        profiles: [{ email, name }],
+        data: {
+          type: "profile-bulk-import-job",
+          attributes: {
+            profiles: {
+              data: [
+                {
+                  type: "profile",
+                  attributes: {
+                    email: email,
+                    first_name: name,
+                    organization: website,
+                  },
+                  meta: { patch_properties: { unset: "skus" } },
+                },
+              ],
+            },
+          },
+          relationships: { lists: { data: [{ type: "list", id: listId }] } },
+        },
       }),
     };
-    const listId =
-      type === "contact" ? contact_form_list_id : free_consultation;
+
     const response = await fetch(
-      "https://a.klaviyo.com/api/v2/list/" +
-        listId +
-        "/subscribe?api_key=" +
-        api_key,
+      `https://a.klaviyo.com/api/profile-bulk-import-jobs/`,
       options
     );
 
@@ -50,11 +47,15 @@ const handler = async (req, res) => {
       const data = await response.json();
       res.status(200).json(data);
     } else {
-      throw new Error("Error adding profile to Klaviyo list");
+      const errorData = await response.json();
+      console.error("Error Response Data:", errorData);
+      throw new Error(
+        `Error adding profile to Klaviyo list: ${errorData.detail}`
+      );
     }
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Server error" });
+    console.error("Catch Block Error:", error);
+    res.status(500).json({ error: error.message || "Server error" });
   }
 };
 
