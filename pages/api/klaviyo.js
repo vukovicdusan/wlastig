97c091a9bcf484cf8d255730672cc337e8d62f64
dusan <dusan.v.vukovic@gmail.com>
2024-07-17 10:31:26 +0200

    Changed klaviyo api calls

--- pages/api/klaviyo.js
+++ pages/api/klaviyo.js
@@ -4,7 +4,7 @@ const handler = async (req, res) => {
   const free_consultation = "XZQiee";
 
   try {
-    const { name, email, type, website } = req.body.data;
+    const { name, email, type, website, comments } = req.body.data;
     const listId =
       type === "contact" ? contact_form_list_id : free_consultation;
     const options = {
@@ -17,35 +17,59 @@ const handler = async (req, res) => {
       },
       body: JSON.stringify({
         data: {
-          type: "profile-bulk-import-job",
+          type: "profile",
           attributes: {
-            profiles: {
-              data: [
-                {
-                  type: "profile",
-                  attributes: {
-                    email: email,
-                    first_name: name,
-                    organization: website,
-                  },
-                  meta: { patch_properties: { unset: "skus" } },
-                },
-              ],
-            },
+            email: email,
+            first_name: name,
+            organization: website,
+            properties: { message: comments, name: name },
           },
-          relationships: { lists: { data: [{ type: "list", id: listId }] } },
         },
       }),
     };
 
     const response = await fetch(
-      `https://a.klaviyo.com/api/profile-bulk-import-jobs/`,
+      `https://a.klaviyo.com/api/profiles/`,
       options
     );
 
     if (response.ok) {
+      const listId =
+        type === "contact" ? contact_form_list_id : free_consultation;
       const data = await response.json();
-      res.status(200).json(data);
+      let profileId = data.data.id;
+      // res.status(200).json(data);
+      const addToListOptions = {
+        method: "POST",
+        headers: {
+          accept: "application/json",
+          revision: "2024-07-15",
+          "content-type": "application/json",
+          Authorization: `Klaviyo-API-Key ${api_key}`,
+        },
+        body: JSON.stringify({
+          data: [
+            {
+              type: "profile",
+              id: profileId, // Use the profile ID in the request body
+            },
+          ],
+        }),
+      };
+      const addToListResponse = await fetch(
+        `https://a.klaviyo.com/api/lists/${listId}/relationships/profiles/`,
+        addToListOptions
+      );
+      if (addToListResponse.ok) {
+        const listData = await addToListResponse.json();
+        res.status(200).json(listData);
+      } else {
+        const errorData = await addToListResponse.json();
+        console.error("Error addToList Response Data: ", errorData);
+        throw new Error(
+          `Error adding profile to Klaviyo list: ${errorData.detail}`
+        );
+      }
     } else {
       const errorData = await response.json();
       console.error("Error Response Data:", errorData);
