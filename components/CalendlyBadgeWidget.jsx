70a900cfeba4a248f221056e6bf270bd74405271
Picvajzmen <picvajzmen@gmail.com>
2024-09-24 10:48:06 +0200

    Added calendly badge widget

---
+++ components/CalendlyBadgeWidget.jsx
@@ -0,0 +1,39 @@
+import { useEffect } from "react";
+import Head from "next/head";
+
+const CalendlyBadgeWidget = () => {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://assets.calendly.com/assets/external/widget.js";
+    script.async = true;
+    document.body.appendChild(script);
+
+    script.onload = () => {
+      if (window.Calendly) {
+        window.Calendly.initBadgeWidget({
+          url: "https://calendly.com/wlastig/free-consultation",
+          text: "Schedule Free Consultation",
+          color: "#f2545b",
+          textColor: "#ffffff",
+          branding: undefined,
+        });
+      }
+    };
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <link
+          href="https://assets.calendly.com/assets/external/widget.css"
+          rel="stylesheet"
+        />
+      </Head>
+    </>
+  );
+};
+
+export default CalendlyBadgeWidget;
--- components/layout/Layout.jsx
+++ components/layout/Layout.jsx
@@ -1,30 +1,32 @@
-import React from "react"
-import styled from "styled-components"
-import Header from "../Header"
-import Footer from "../Footer"
+import React from "react";
+import styled from "styled-components";
+import Header from "../Header";
+import Footer from "../Footer";
+import CalendlyBadgeWidget from "../CalendlyBadgeWidget";
 
 const Layout = ({ children }) => {
-	return (
-		<LayoutStyled>
-			<Header></Header>
-			{children}
-			<Footer></Footer>
-		</LayoutStyled>
-	)
-}
+  return (
+    <LayoutStyled>
+      <CalendlyBadgeWidget></CalendlyBadgeWidget>
+      <Header></Header>
+      {children}
+      <Footer></Footer>
+    </LayoutStyled>
+  );
+};
 
 export const LayoutStyled = styled.div`
-	display: flex;
-	flex-direction: column;
-	height: 100vh;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
 
-	& > *:nth-child(2) {
-		flex-grow: 1;
-	}
+  & > *:nth-child(2) {
+    flex-grow: 1;
+  }
 
-	& > *:last-child {
-		flex-shrink: 0;
-	}
-`
+  & > *:last-child {
+    flex-shrink: 0;
+  }
+`;
 
-export default Layout
+export default Layout;
