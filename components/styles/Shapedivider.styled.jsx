c3f7f50c311ad93769021025d3f74a05b7d64995
dusan <dusan.v.vukovic@gmail.com>
2024-10-21 18:39:40 +0200

    Small shapedivider fix

--- components/styles/Shapedivider.styled.jsx
+++ components/styles/Shapedivider.styled.jsx
@@ -1,29 +1,29 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 export const Shapedivider = styled.div`
-	position: absolute;
-	top: ${(props) => (props.position === "top" ? 0 : "revert")};
-	left: 0;
-	bottom: ${(props) => (props.position === "bottom" ? 0 : "revert")};
-	width: 100%;
-	overflow: hidden;
-	line-height: 0;
-	transform: rotate(${(props) => props.rotation || "180"}deg);
+  position: absolute;
+  top: ${(props) => (props.position === "top" ? "-1px" : "revert")};
+  left: 0;
+  bottom: ${(props) => (props.position === "bottom" ? "-1px" : "revert")};
+  width: 100%;
+  overflow: hidden;
+  line-height: 0;
+  transform: rotate(${(props) => props.rotation || "180"}deg);
 
-	& svg {
-		position: relative;
-		display: block;
-		width: calc(100% + 1.3px);
-		height: ${(props) => props.height || "150px"};
-	}
+  & svg {
+    position: relative;
+    display: block;
+    width: calc(100% + 1.3px);
+    height: ${(props) => props.height || "150px"};
+  }
 
-	& svg path {
-		fill: ${(props) => props.fill || "var(--primary)"};
-	}
+  & svg path {
+    fill: ${(props) => props.fill || "var(--primary)"};
+  }
 
-	@media (max-width: 600px) {
-		& svg {
-			height: 80px;
-		}
-	}
-`
+  @media (max-width: 600px) {
+    & svg {
+      height: 80px;
+    }
+  }
+`;
