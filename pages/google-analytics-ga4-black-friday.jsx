bf9b7fe6dfe763c23f924ab6e8c865394f0ec7a2
Picvajzmen <picvajzmen@gmail.com>
2024-10-29 14:21:57 +0100

    Scroll links fixes on bf landing

--- pages/google-analytics-ga4-black-friday.jsx
+++ pages/google-analytics-ga4-black-friday.jsx
@@ -484,12 +484,11 @@ const Landing = () => {
               <path d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z"></path>
             </svg>
           </Shapedivider>
-          <Region pt={"12rem"} pb={"12rem"}>
+          <Region id="freeConsultation-bf" pt={"12rem"} pb={"12rem"}>
             <Wrapper>
               <AnimationContainer>
                 <Stack stackJustify={"center"} stackAlign={"center"}>
                   <StyledText
-                    id="freeConsultation-bf"
                     color={"var(--text-light)"}
                     as={"h3"}
                     className="title-xl text-center"
