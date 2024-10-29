b33206d14afbdf6f46b54efa004c440ed01480a5
dusan <dusan.v.vukovic@gmail.com>
2024-10-29 07:41:25 +0100

    Edited positioning of svgs in bg, and added some more to the section why-us

--- pages/google-analytics-ga4-black-friday.jsx
+++ pages/google-analytics-ga4-black-friday.jsx
@@ -394,7 +394,13 @@ const Landing = () => {
               <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"></path>
             </svg>
           </Shapedivider>
-          <Region pt={"7rem"}>
+          <Region id="why-us" pt={"7rem"}>
+            <svg className="bf-icon">
+              <use xlinkHref={"./img/bf-sprite.svg#shopping-bag"}></use>
+            </svg>
+            <svg className="bf-icon">
+              <use xlinkHref={"./img/bf-sprite.svg#rocket2"}></use>
+            </svg>
             <Wrapper>
               <Stack
                 stackSpace={"var(--s4)"}
@@ -610,6 +616,14 @@ export const BFLandingStyled = styled.div`
   .bf-icon:nth-child(3) {
     top: -5%;
     left: 70%;
+  }
+  #why-us .bf-icon:nth-child(1) {
+    top: 5%;
+    left: 0;
+  }
+  #why-us .bf-icon:nth-child(2) {
+    top: 15%;
+    left: 80%;
   }
 
   @-moz-keyframes slowSpin {
