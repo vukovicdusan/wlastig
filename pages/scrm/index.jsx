91a20f8f0fefd60f5115e59e32a9210a3e5df1b3
Dusan <dusan.v.vukovic@gmail.com>
2025-10-28 20:59:00 +0100

    Added stagging crm to the scrm page

--- pages/scrm/index.jsx
+++ pages/scrm/index.jsx
@@ -15,6 +15,16 @@ const SCRM = () => {
         <meta name="robots" content="noindex, nofollow" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Script
+        src="https://staging.simplecrm.xyz/embed.js"
+        strategy="afterInteractive"
+        onLoad={() => {
+          try {
+            window.scrm?.init?.();
+          } catch {}
+        }}
+      />
+
       <Region>
         <Wrapper>
           <ScrmStagingStyled>
@@ -22,22 +32,11 @@ const SCRM = () => {
               <h1 className="center">Simple CRM Submission Test Page</h1>
               <Stack stackAlign={"center"}>
                 <h2>External Form</h2>
-                <ContactForm
-                  bfBg={"--background-light"}
-                  formType={"freeConsultation2"}
-                  btnId={"form_submit_consultation2"}
-                  scrmIsStaging={true}
-                ></ContactForm>
+                <ContactForm bfBg={"--background-light"} formType={"freeConsultation2"} btnId={"form_submit_consultation2"} scrmIsStaging={true}></ContactForm>
               </Stack>
               <Stack stackAlign={"center"}>
                 <h2>Classic Form</h2>
-                <iframe
-                  frameBorder={0}
-                  title="Vlada Wlastig Testing Form 2"
-                  src="https://staging.simplecrm.xyz/view-form?publicID=14e6e9e2-2576-404c-8374-0779998efef6"
-                  width="100%"
-                  height="100%"
-                ></iframe>
+                <iframe frameBorder={0} title="Vlada Wlastig Testing Form 2" src="https://staging.simplecrm.xyz/view-form?publicID=14e6e9e2-2576-404c-8374-0779998efef6" width="100%" height="100%"></iframe>
               </Stack>
             </Stack>
           </ScrmStagingStyled>
